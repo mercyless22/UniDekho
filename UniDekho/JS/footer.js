b0417db1306091ef7ed3e1190917f2e61dbb3092
Taher Travadi <115689584+mercyless22@users.noreply.github.com>
2024-09-27 19:05:00 +0530

    Delete UniDekho directory

--- UniDekho/JS/footer.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('subscribe-btn').addEventListener('click', function() {
-    const emailInput = document.getElementById('email-input').value;
-    if (emailInput) {
-        alert(`Subscribed successfully with email: ${emailInput}`);
-    } else {
-        alert('Please enter a valid email address.');
-    }
-});
